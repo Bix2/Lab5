9a52ee8c1ae5827eef381b7f40e4944016e9f2d1
Bix2 <bibi.treffers@outlook.com>
2018-03-23 22:44:36 +0100

    changed var into const, created variable mongoose and body-parser, made connection to mongo db polls on localhost

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require ('body-parser');
+const mongoose = require('mongoose');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+// mongoose
+mongoose.connect('mongodb://localhost/polls');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.json());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
