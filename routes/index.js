33ab349bee18405b15d1d65b7cacb470f47f1b9c
Bix2 <bibi.treffers@outlook.com>
2018-03-24 01:12:04 +0100

    post request on createpoll

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,19 @@ var router = express.Router();
 const Poll = require('./../models/Poll');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Poll' });
+});
+
+router.post('/createpost', (req, res, next) => {
+  let pollData = new Poll(req.body);
+  pollData.save()
+    .then(item => {
+      res.send("Name saved to database");
+    })
+    .catch(err => {
+      res.status(400).send("Unable to save to database");
+    });
 });
 
 module.exports = router;
