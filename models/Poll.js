6738b48b4a63349362bddb9fd1760a2e183ede9a
Bix2 <bibi.treffers@outlook.com>
2018-03-26 16:27:20 +0200

    added choiceSchema

--- models/Poll.js
+++ models/Poll.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-var pollSchema = new Schema({
-    question: String,
-    choice1: String,
-    choice2: String,
-    votes: {
-      type: Number,
-      default: 0
-    }
+var choiceSchema = new Schema({
+  text: String,
+  votes: {
+    type: Number,
+    default: 0
+  }
 });
 
+var pollSchema = new Schema({
+    question: String,
+    choices: [choiceSchema]
+});
+
+/*
 pollSchema.method('vote', function voting(vote, cb) {
   this.votes += 1;
   this.parent().save(cb);
-});
+});*/
 
 const Poll = mongoose.model('Poll', pollSchema);
 
