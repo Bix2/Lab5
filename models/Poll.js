0a1c02db2f5418381109b28e9c38b08b11dc46d3
Bix2 <bibi.treffers@outlook.com>
2018-03-26 15:25:04 +0200

    poll schema and voting function

--- models/Poll.js
+++ models/Poll.js
@@ -1,28 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-var answerSchema = new Schema({
-    answer: {
-      type: String,
-      unique: true
-    },
+var pollSchema = new Schema({
+    question: String,
+    choice1: String,
+    choice2: String,
     votes: {
       type: Number,
       default: 0
     }
 });
 
-answerSchema.method('vote', function voting(vote, cb) {
-    this.votes += 1;
-    this.parent().save(cb);
-});
-
-var pollSchema = new Schema({
-    question: {
-      type: String,
-      unique: true
-    },
-    answers: [answerSchema]
+pollSchema.method('vote', function voting(vote, cb) {
+  this.votes += 1;
+  this.parent().save(cb);
 });
 
 const Poll = mongoose.model('Poll', pollSchema);
