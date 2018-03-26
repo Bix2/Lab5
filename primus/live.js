b3d49a22407f877a2cb4062f83fba7285043b22b
Bix2 <bibi.treffers@outlook.com>
2018-03-26 13:42:25 +0200

    start listening for connections and emit data event when message from client is received

--- primus/live.js
+++ primus/live.js
@@ -4,6 +4,16 @@ exports.go = function(server) {
     const Primus = require('primus');
     let primus = new Primus(server, {});
 
-    primus.library();
-    primus.save(__dirname + '/primus.js');
-}
+    //primus.library();
+    //primus.save(__dirname + '/primus.js');
+
+    // start listening for connections
+    primus.on('connection', (spark) => {
+        console.log("Spark connected")
+
+        // data event is emitted when a message is received from the client.
+        spark.on('data', (data) => {
+            primus.write(data); 
+        })
+    })
+};
