23f5d3a421cbdb76baba8b131b5e3c98c73d2b99
Bix2 <bibi.treffers@outlook.com>
2018-03-29 01:14:07 +0200

    made the schema more easy (but less dynamic) because input wasnt stored correctly in database

--- models/Poll.js
+++ models/Poll.js
@@ -1,24 +1,31 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-var choiceSchema = new Schema({
-  text: String,
-  votes: {
-    type: Number,
-    default: 0
+const pollSchema = new Schema({
+  question: String,
+  updated_at: Date,
+  choice1: {
+    text: String,
+    votes: {
+      type: Number,
+      default: 0
+    }
+  },
+  choice2: {
+    text: String,
+    votes: {
+      type: Number,
+      default: 0
+    }
   }
-});
-
-var pollSchema = new Schema({
-    question: String,
-    choices: [choiceSchema]
-});
+})
 
 /*
 pollSchema.method('vote', function voting(vote, cb) {
   this.votes += 1;
   this.parent().save(cb);
-});*/
+});
+*/
 
 const Poll = mongoose.model('Poll', pollSchema);
 
